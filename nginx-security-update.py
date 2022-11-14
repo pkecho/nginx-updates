b2826d2a924b557638f5e38e1068258b9b62f58f
pkecho <ubuntu@PK-Desktop.localdomain>
2022-11-13 22:59:16 -0800

    changes

--- nginx-security-update.py
+++ nginx-security-update.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python
+
+__author__ = 'pk31337'
+__version__ = '1.0.1'
+__maintainer__ = 'pk31337'
+__status__ = 'Development'
+
 from bs4 import BeautifulSoup
+from datetime import date
 import requests
 import os
 
-CHANGES = 'Changes with nginx'
-SECURITY = 'Security:'
+changes = 'Changes with nginx'
+security = 'Security:'
 source = requests.get('http://nginx.org/en/CHANGES').text
-soup = BeautifulSoup(source, 'html5lib')
-logs = soup.find('body')
+soup = BeautifulSoup(source, 'lxml')
+logs = soup.body.p.text
+year = date.today().year
 
 
-with open('/tmp/test.txt', 'w') as file:
-    file.write(logs.prettify())
+def write_log():
+    with open('/tmp/test.txt', 'w') as file:
+        file.write(logs)
 
-with open('test.txt', 'r') as file:
-    for line in file:
-        if CHANGES in line:
-            line = line.split()
-            nversion = line[3]
-            ndate = line[4] + ' ' + line[5] + ' ' + line[6]
-            print(nversion + ' ' + ndate)
-        
-        if SECURITY in line:
-            print(nversion + ' had a security update')
+def version_changes():
+    with open('/tmp/test.txt', 'r') as file:
+        for line in file:
+            if changes in line:
+                line = line.split()
+                if int(line[6]) == int(year):
+                    nversion = line[3]
+                    ndate = line[4] + ' ' + line[5] + ' ' + line[6]
+                    print(nversion + ' ' + ndate)
+            if security in line:
+                print(nversion + ' had a security update')
 
-os.remove("/tmp/test.txt")
+def main():
+    write_log()
+    version_changes()
+    # Remove if you want to keep the logs
+    os.remove("/tmp/test.txt")
+
+if __name__ == '__main__':
+    main()
